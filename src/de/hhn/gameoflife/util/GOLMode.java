6dedd50bf763b7ae8fdbfe3e99c2711b80b49662
BLVDE <00nvogel@gmail.com>
2023-05-12 11:24:15 +0200

    Removed Step

--- src/de/hhn/gameoflife/util/GOLMode.java
+++ src/de/hhn/gameoflife/util/GOLMode.java
@@ -1,7 +1,6 @@
 package de.hhn.gameoflife.util;
 
 public enum GOLMode {
-    STEP,
     RUN,
     DRAW,
     SET;
@@ -9,7 +8,6 @@ public enum GOLMode {
     @Override
     public String toString() {
         return switch (this) { //TODO String Resources
-            case STEP -> "Schritt";
             case RUN -> "Laufen";
             case DRAW -> "Malen";
             case SET -> "Setzen";
