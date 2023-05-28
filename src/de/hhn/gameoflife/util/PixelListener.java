c81faefd00cd5c2f112337c39e8f88ed1c0bd7dc
BLVDE <00nvogel@gmail.com>
2023-05-28 13:44:29 +0200

    Added Pixel interface and placeholder for line

---
+++ src/de/hhn/gameoflife/util/PixelListener.java
@@ -0,0 +1,5 @@
+package de.hhn.gameoflife.util;
+
+public interface PixelListener {
+    void pixelEvent(int x, int y);
+}
---
+++ src/de/hhn/gameoflife/util/PixelStreams.java
@@ -0,0 +1,7 @@
+package de.hhn.gameoflife.util;
+
+public class PixelStreams {
+    public static void forEachPixelInLine(int x0, int y0, int x1, int y1, PixelListener listener) {
+
+    }
+}
